f95a4d3560fd7a6829598852918d5ec748f793c3
f-gdovin <f.gdovin@gmail.com>
2015-03-29 00:44:20 +0100

    15.03.29
    sleep-generated timing of sending, also generating current timestamp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -13,9 +18,10 @@ public class Main {
 
     public static void main(String[] args){
 
-        Map schema;
+        int sleepTime = 3000; //time between events in ms
 
         File myFile = new File("C:\\jsonInput.json");
+        File myFile2 = new File("C:\\jsonInput2.json");
 
         if(args.length > 0){ //definovanie vlastneho suboru ako 1. vstupny parameter
             myFile = new File(args[0]);
@@ -24,21 +30,44 @@ public class Main {
         try{
             FileReader inputFile = new FileReader(myFile);
             BufferedReader bufferReader = new BufferedReader(inputFile);
-
             String line;
 
             //send contents of file
             System.out.println("Sending events from file " + myFile.getAbsolutePath().toString()+ "...");
 
             while ((line = bufferReader.readLine()) != null){
-                System.out.println(line);
-                RabbitMQsender.publish(line);
+                Thread.sleep(sleepTime);
+                String toSend = addTimestamp(line);
+                RabbitMQsender.publish("esperQueue", "logs", toSend);
+                System.out.println("Sending '" + toSend + "'");
             }
             bufferReader.close();
+
+
+            /*FileReader inputFile2 = new FileReader(myFile2);
+            BufferedReader bufferReader2 = new BufferedReader(inputFile2);
+            String line2;
+
+            //send contents of second file
+            System.out.println("Sending events from file " + myFile2.getAbsolutePath().toString()+ "...");
+
+            while ((line2 = bufferReader2.readLine()) != null){
+                System.out.println(line2);
+                RabbitMQsender.publish("esperQueue2", "logs2", line2);
+            }
+            bufferReader2.close();*/
         }
         catch(Exception ex){
             Logger.getLogger(Main.class.getName()).log(Level.WARN, "Error while reading file line by line:", ex);
         }
         System.out.println("Everything sent without errors\n");
+    }
+
+    private static String addTimestamp(String line) {
+        String currTime = LocalDateTime.now().toString();
+        String pattern = "generateTimestampHere";
+        int index = line.indexOf(pattern);
+        String result = line.substring(0, index) + currTime + line.substring(index + pattern.length());
+        return result;
     }
 }
--- src/main/java/RabbitMQsender.java
+++ src/main/java/RabbitMQsender.java
@@ -15,10 +15,12 @@ import com.rabbitmq.client.MessageProperties;
  */
 public class RabbitMQsender {
 
-    private static final String EXCHANGE_NAME = "logs";
-    private final static String QUEUE_NAME = "testQueue";
+    private static String EXCHANGE_NAME = "logs";
+    private static String QUEUE_NAME = "esperQueue";
 
-    public static void publish(String message) throws Exception {
+    public static void publish(String queueName, String exchangeName, String message) throws Exception {
+        QUEUE_NAME = queueName;
+        EXCHANGE_NAME = exchangeName;
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
@@ -26,8 +28,8 @@ public class RabbitMQsender {
         Channel channel = connection.createChannel();
 
         channel.exchangeDeclare(EXCHANGE_NAME, "fanout", true); //fanout ignoruje routing kluce, potom zmenit!
-        String queueName = channel.queueDeclare(QUEUE_NAME, true, false, true, null).getQueue();
-        channel.queueBind(queueName, EXCHANGE_NAME, "");
+        String givenQueueName = channel.queueDeclare(QUEUE_NAME, false, false, false, null).getQueue();
+        channel.queueBind(givenQueueName, EXCHANGE_NAME, "");
         channel.basicPublish(EXCHANGE_NAME, "", MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
 
 
