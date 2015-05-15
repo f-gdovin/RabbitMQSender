2838d614b5817533e43a4117e08baff91d9dc43f
f-gdovin <f.gdovin@gmail.com>
2015-05-15 12:32:47 +0200

    15.05.15
    changed to take params from command line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,77 +1,12 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-
 /**
  * @author Filip Gdovin
  * @version 21. 1. 2015
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int sleepTime = 3000; //time between events in ms for first stream
-
-        File myFile = loadFileFromResources("jsonInput.json");
-        File myFile2 = loadFileFromResources("jsonInput2.json");
-
-        if(args.length > 0){
-            myFile = new File(args[0]);
-        }
-
-        try{
-            //send contents of file
-            FileReader inputFile = new FileReader(myFile);
-            BufferedReader bufferReader = new BufferedReader(inputFile);
-            String line = bufferReader.readLine();
-
-            //send contents of second file
-            FileReader inputFile2 = new FileReader(myFile2);
-            BufferedReader bufferReader2 = new BufferedReader(inputFile2);
-            String line2 = null; //bufferReader2.readLine();  //null to disable second stream
-
-            do {
-//                Thread.sleep(sleepTime);
-                if(line != null) {
-                    String toSend = addTimestamp(line);
-                    RabbitMQsender.publish("inputQueue", toSend); //will be dropped till queue is declared (so, declare)
-                    System.out.println("Sending '" + toSend + "' from file " + myFile.getAbsolutePath());
-                    line = bufferReader.readLine();
-                }
-
-                if(line2 != null) {
-                    RabbitMQsender.publish("inputQueue2", line2); //will be dropped till queue is declared (after that it will be rejected as malformed (that file doesn't contain timestamp))
-                    System.out.println("Sending '" + line2 + "' from file " + myFile2.getAbsolutePath());
-                    line2 = bufferReader2.readLine();
-                }
-            } while ((line != null) || (line2 != null));
-            bufferReader.close();
-            bufferReader2.close();
-        }
-        catch(Exception ex){
-            System.out.println("Error while reading file line by line: " + ex.getMessage() + ex.getCause());
-            return;
-        }
-        System.out.println("Everything sent without errors\n");
-    }
-
-    private static File loadFileFromResources(String fileName) throws NullPointerException{
-        return new File(RabbitMQsender.class.getClassLoader().getResource(fileName).getFile());
-    }
-
-    private static String addTimestamp(String line) {
-        final DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-        ZonedDateTime localTime = LocalDateTime.now().atZone(ZoneId.of("+02:00"));
-        String currTimeString = localTime.format(formatter);
-        String pattern = "generateTimestampHere";
-        int index = line.indexOf(pattern);
-        if(index < 0) {
-            return line;
-        }
-        return line.substring(0, index) + currTimeString + line.substring(index + pattern.length());
+        RabbitMQsender firstSender = new RabbitMQsender();
+        firstSender.send(args);
     }
 }
--- src/main/java/RabbitMQsender.java
+++ src/main/java/RabbitMQsender.java
@@ -9,14 +9,74 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  *
  * @author Filip Gdovin, 410328
  */
 public class RabbitMQsender {
 
-    public static void publish(String routingKey, String message) throws Exception {
-        String EXCHANGE_NAME = "amq.direct";
+    public void send(String[] args) {
+
+        if(args.length != 3) {
+            System.out.println("Bad number of arguments, Sender needs String queueName, String fileName, int sleepTime");
+        }
+
+        String queueName = args[0];
+        String fileName = args[1];
+        int sleepTime = Integer.parseInt(args[2]);
+
+        File myFile = new File(fileName);
+
+        try{
+            //send contents of file
+            FileReader inputFile = new FileReader(myFile);
+            BufferedReader bufferReader = new BufferedReader(inputFile);
+            String line = bufferReader.readLine();
+
+            do {
+                Thread.sleep(sleepTime);
+                if(line != null) {
+                    String toSend = addTimestamp(line);
+                    this.publish(queueName, toSend); //will be dropped till queue is declared (so, declare)
+                    System.out.println("Sending '" + toSend + "' from file " + myFile.getAbsolutePath());
+                    line = bufferReader.readLine();
+                }
+            } while (line != null);
+            bufferReader.close();
+        }
+        catch(Exception ex){
+            System.out.println("Error while reading file line by line: " + ex.getMessage() + ex.getCause());
+            return;
+        }
+        System.out.println("Everything sent without errors\n");
+    }
+
+    private static File loadFileFromResources(String fileName) throws NullPointerException{
+        return new File(RabbitMQsender.class.getClassLoader().getResource(fileName).getFile());
+    }
+
+    private static String addTimestamp(String line) {
+        final DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        ZonedDateTime localTime = LocalDateTime.now().atZone(ZoneId.of("+02:00"));
+        String currTimeString = localTime.format(formatter);
+        String pattern = "generateTimestampHere";
+        int index = line.indexOf(pattern);
+        if(index < 0) {
+            return line;
+        }
+        return line.substring(0, index) + currTimeString + line.substring(index + pattern.length());
+    }
+
+    private void publish(String routingKey, String message) throws Exception {
+        String EXCHANGE_NAME = "esperExchange";
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
